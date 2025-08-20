fc0bb8b710d2322e22780a70b1e40991f7e247e2
lessly539 <lesslyhass@gmail.com>
2025-08-20 04:37:23 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~oj9jCD7Z#OtWRZTzdMDO12DpNFKwKAuw5pWTme_mE3KdSY5nwKx4",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "254755319195", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
